fb25cb6a44b7a8bcb9b64cc92d8476b10a5fefbd
Assem elnahas <151422755+AssemElnahas@users.noreply.github.com>
2026-09-13 16:57:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from app.agent import ask_agent
+
+app = FastAPI(
+    title="Grmini-AI-Agent-API",
+    version = "0.1"
+)
+
+class ChatRequest(BaseModel):
+    message: str
+
+class ChatResponse(BaseModel):
+    response: str
+
+@app.get("/")
+def root():
+    return{
+        "message": "Gemini Agent API is running!"
+    }
+
+@app.post("/api/chat", response_model=ChatResponse)
+def chat(request: ChatRequest):
+    answer = ask_agent(request.message)
+    
+    return {
+        "response: answer"
+    }
+    
